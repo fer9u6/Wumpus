8479cf8db115e2f9bb9669db9ef50f7f8dc6dd4a
mariaferc96 <mariaferc96@hotmail.com>
2017-09-30 11:14:26 -0600

    Genera cuevas y se pueden mover

--- app/src/main/java/isl/wumpus/MainActivity.java
+++ app/src/main/java/isl/wumpus/MainActivity.java
@@ -5,13 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static Lienzo lienzo;
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        lienzo = (Lienzo) findViewById(R.id.lienzo);
-
     }
 }
