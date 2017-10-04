a90900b8b66b383dff6929587bec5b0951aa43fa
afeoli <afeoli07@gmail.com>
2017-10-03 23:47:49 -0600

    Agrega read y write, avance en pruebas

--- app/src/main/java/isl/wumpus/MainActivity.java
+++ app/src/main/java/isl/wumpus/MainActivity.java
@@ -1,15 +1,20 @@
 package isl.wumpus;
 
+        import android.content.Context;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    static Context appContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        appContext = getApplicationContext();
+
+    }
 
 }
