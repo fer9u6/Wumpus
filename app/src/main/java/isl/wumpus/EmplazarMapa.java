617f36c926a4b87066fa28f32d360d1448f50386
KevinWV <kevin.waltam@gmail.com>
2017-10-25 18:00:24 -0600

    Emplaza mapas final

--- app/src/main/java/isl/wumpus/EmplazarMapa.java
+++ app/src/main/java/isl/wumpus/EmplazarMapa.java
@@ -76,8 +76,8 @@ public class EmplazarMapa extends FragmentActivity implements OnMapReadyCallback
         GestionadorDeArchivos ga = new GestionadorDeArchivos();
         String s =ga.read(nombreMapa,getApplicationContext());
         mapaWumpus= ga.convertirStringAObjeto(s);
-
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -123,29 +123,42 @@ public class EmplazarMapa extends FragmentActivity implements OnMapReadyCallback
 
     //por ahora fija el punto y agrega una cantidad de cuevas , agrega varias en el mismo punto , ver etiquetas(pasa algo raro)
     private void fijaPunto(){
-        if(marker.isVisible()) { // se podria hacer una mejor validacion
+        if(marker.isVisible()) { // se podria hacer una mejor validacion  R: si....
             puntoFijo = true;
             marker.setDraggable(false);
         }
+        int[] cuevaX= mapaWumpus.getCuevaX();
+        int[] cuevaY= mapaWumpus.getCuevaY();
+        int[] caminoV1= mapaWumpus.getCaminoV1();
+        int[] caminoV2 = mapaWumpus.getCaminoV2();
+        for(int o=1; o<cuevaX.length; o++){
+            cuevaX[o]-= cuevaX[0];
+            cuevaY[o]-= cuevaX[0];
+        }
+
         //colocar los demas marcadores
        marcadores.add(marker); //marker es la primera cueva
         if(idMapaReg!=0){
             //asignarMapaReg();
         }
-        int cantidadCuevas=4; //por ahora fijo porque mapawumpus no sirve
-        for(int i =0;i<cantidadCuevas;i++) {
-            double lat = marcadores.get(i).getPosition().latitude;
-            double lon = marcadores.get(i).getPosition().longitude;
-            LatLng coord = new LatLng(lat, lon);
-            Marker m = mMap.addMarker(new MarkerOptions().position(coord).title("x")
-                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.cueva)).draggable(true));
-            marcadores.add(m);
+        int cantidadCuevas=mapaWumpus.getContCuevas(); //por ahora fijo porque mapawumpus no sirve
+        for(int i =1;i<cantidadCuevas;i++) {
+            double lat = marcadores.get(0).getPosition().latitude;
+            double lon = marcadores.get(0).getPosition().longitude;
+
             // degree in google map is equal to 111.32 Kilometer. 1Degree = 111.32KM. 1KM in Degree = 1 / 111.32 = 0.008983. 1M in Degree = 0.000008983
             // agregar nuevo marcador a 5 metros markers[i]
-            double metros = 5;
-            double coef = metros * 0.0000089;
+            double metros = cuevaX[i];
+            double coef = metros * 0.0000005;
             double new_lat = lat + coef;
+            metros = cuevaY[i];
+            coef = metros * 0.0000003;
             double new_long = lon + coef / Math.cos(lat * 0.018);
+            LatLng coord = new LatLng(new_lat, new_long);
+            Marker m = mMap.addMarker(new MarkerOptions().position(coord).title("x")
+                    .icon(BitmapDescriptorFactory.fromResource(R.mipmap.cueva)).draggable(false));
+
+            marcadores.add(m);
             agregarOtroMarcador(new_lat, new_long, m, ""+(i+2)+""); //empieza poniendo de titulo cueva 2
         }
 
